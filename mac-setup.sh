9006985958e70f0461d070032d4326abc7ef3436
Thomas Seropian <thomas@skinanalytics.co.uk>
2017-10-16 11:40:22 +0100

    More softs

---
+++ mac-setup.sh
@@ -0,0 +1,16 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+
+brew cask install atom
+brew cask install iterm2
+brew cask install google-drive-file-stream
+brew cask install postman
+brew cask install sequel-pro
+brew cask install gas-mask
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install vagrant-manager
+brew install node
+brew install zsh
+brew install git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
